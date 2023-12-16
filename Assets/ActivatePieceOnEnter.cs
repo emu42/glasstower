661f77ade7c0349f2534568b9023d20128a75c77
emu42 <edmund.urbani@LilandIT.com>
2023-12-16 21:45:11 +0100

    displays

--- Assets/ActivatePieceOnEnter.cs
+++ Assets/ActivatePieceOnEnter.cs
@@ -13,7 +13,7 @@ public class ActivatePieceOnEnter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame();
     }
 
     // Update is called once per frame
